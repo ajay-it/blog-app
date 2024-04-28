d7fd88fdecbbcea6c98cb3897e2b5c06559452fa
Ajay Kushwaha <ajkushwaha1112@gmail.com>
2024-04-28 20:13:13 +0530

    created signup api

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,32 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    if (
+      !username ||
+      !email ||
+      !password ||
+      username === "" ||
+      email === "" ||
+      password === ""
+    ) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    res.json({ message: "Signup successful" });
+  } catch (error) {
+    console.error("Error in signup:", error.message);
+    return res.status(500).json({ message: error.message });
+  }
+};
