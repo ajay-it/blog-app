c036e0996f0d53c6483c5777a368e2d819a59f45
Ajay Kushwaha <ajkushwaha1112@gmail.com>
2024-05-01 23:40:54 +0530

    profile page ui

--- blog-ui/src/pages/Dashboard.jsx
+++ blog-ui/src/pages/Dashboard.jsx
@@ -1,3 +1,25 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../componets/DashSidebar";
+import DashProfile from "../componets/DashProfile";
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState();
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
+  return (
+    <div className="min-h-screen flex flex-col sm:flex-row items-center sm:items-start">
+      <div>
+        <DashSidebar />
+      </div>
+      {tab === "profile" && <DashProfile />}
+    </div>
+  );
 }
